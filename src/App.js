2aab9177af4d8fc37366bc2ed23330b1e0b23645
Gopala <guptaraja727@gmail.com>
2022-09-14 19:21:47 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Routes, Route, BrowserRouter } from "react-router-dom";
+import Home from "./Home";
+import SingleMovie from "./SingleMovie";
+import "./App.css";
+
+const App = () => {
+  return (
+    
+    <Routes>
+      <Route path="/" element={<Home />} />
+      <Route path="movie/:id" element={<SingleMovie />} />
+    </Routes>
+  
+  );
+};
+
+export default App;
